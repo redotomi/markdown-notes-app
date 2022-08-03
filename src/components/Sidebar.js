48b740f87f0068025896b97d1a0ebe7aa99de4ac
redotomi <tomy.redondo001@gmail.com>
2022-08-03 17:32:17 -0300

    Crea botón de eliminar nota

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,6 +12,12 @@ export default function Sidebar(props) {
         onClick={() => props.setCurrentNoteId(note.id)}
       >
         <h4 className="text-snippet">{`${note.body.split("\n")[0]}` || '-Titleless note-'}</h4>
+        <button
+          className="delete-btn"
+          onClick={(ev) => { props.delete(ev, note.id) }}
+        >
+          <i className="gg-trash trash-icon"></i>
+        </button>
       </div>
     </div>
   ))
