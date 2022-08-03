76206a94b070a0ba3ec882ada691981a9b5a1619
redotomi <tomy.redondo001@gmail.com>
2022-08-03 16:30:23 -0300

    Agrega pequeñas actualizaciones de diseño

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default function App() {
         notes.length > 0
           ?
           <Split
-            sizes={[30, 70]}
+            sizes={[20, 80]}
             direction="horizontal"
             className="split"
           >
